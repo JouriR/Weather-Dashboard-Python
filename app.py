0a7d95d63273ed44771bbc97e436473ce90d3700
JouriR <jouriroosjen@users.github.com>
2020-07-27 13:23:07 +0200

    V0.1.0

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import requests
+import configparser
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def weather_dashboard():
+    return render_template('index.html')
+
+
+@app.route('/results', methods=['POST'])
+def render_results():
+    zip_code = request.form['zipCode']
+    country_code = request.form['countryCode']
+
+    api_key = get_api_key()
+    data = get_weather_results(zip_code, country_code, api_key)
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+    return render_template('results.html', location=location, temp=temp, feels_like=feels_like, weather=weather)
+
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+
+
+def get_weather_results(zip_code, country_code, api_key):
+    api_url = "https://api.openweathermap.org/data/2.5/weather?zip={},{}&appid={}".format(
+        zip_code, country_code, api_key)
+    r = requests.get(api_url)
+    return r.json()
+
+
+if __name__ == '__main__':
+    app.run()
